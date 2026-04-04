d4e0ced32bb515464779046730e90aa14f9129a9
Grandsummoner <gsnextninja@gmail.com>
2026-04-04 13:49:59 +0800

    Update plugin.hpp

--- src/plugin.hpp
+++ src/plugin.hpp
@@ -1,8 +1,15 @@
 #pragma once
-#include "rack.hpp"
+#include <rack.hpp>
 
 using namespace rack;
 
 extern Plugin* pluginInstance;
-
 extern Model* modelSkyline;
+
+extern float maybeDefaultContrast;
+extern int maybeDefaultTheme;
+
+void maybeLoadSettings();
+void maybeSaveSettings();
+void maybeApplyContrastToAll(float contrast);
+void maybeApplyThemeToAll(int theme);
