69797ccc96d54d8b40477fad50669c394223900c
Grandsummoner <gsnextninja@gmail.com>
2026-06-28 14:47:15 +0800

    Update plugin.cpp

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,8 +1,11 @@
 #include "plugin.hpp"
 
+// Initialize the universal shared plugin binary memory instance
 Plugin* pluginInstance;
 
 void init(Plugin* p) {
     pluginInstance = p;
+
+    // Register corrected structural target execution hooks
     p->addModel(modelSoundscape);
 }
