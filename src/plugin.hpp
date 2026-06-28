53914a75bd6d62e6b8146e6fd7bbe66a9572d0b8
Grandsummoner <gsnextninja@gmail.com>
2026-06-28 14:46:56 +0800

    Update plugin.hpp

--- src/plugin.hpp
+++ src/plugin.hpp
@@ -3,5 +3,8 @@
 
 using namespace rack;
 
+// Declare the universal plugin instance reference
 extern Plugin* pluginInstance;
+
+// Corrected alignment target matching Soundscape architecture
 extern Model* modelSoundscape;
