e39bf1bf9323b8349d1c89c64ccea609ab056a65
Grandsummoner <gsnextninja@gmail.com>
2026-07-09 21:30:24 +0800

    Update soundscapes_sequencer.cpp

--- src/soundscapes_sequencer.cpp
+++ src/soundscapes_sequencer.cpp
@@ -1,14 +1,12 @@
 #include "soundscapes.hpp"
 
 void Soundscapes::processSequencer(const ProcessArgs& args) {
-    // 1. Process Global Buttons State Toggles
     if (btnTriggers[1].process(params[SHFT_PARAM].getValue())) {
         shftMode = !shftMode;
-        if (shftMode) focusedFX = -1; // Symmetrical disengage
+        if (shftMode) focusedFX = -1;
     }
     if (btnTriggers[0].process(params[PLAY_PARAM].getValue())) {
         if (shftMode) {
-            // SHFT + PLAY = Reset playheads
             for(int i = 0; i < 8; i++) {
                 currentSteps[i] = 0;
                 playheadPhases[i] = 0.0f;
@@ -22,7 +20,6 @@ void Soundscapes::processSequencer(const ProcessArgs& args) {
     if (btnTriggers[2].process(params[ARP_PARAM].getValue())) arpMode = !arpMode;
     if (btnTriggers[3].process(params[FRZ_PARAM].getValue())) frzMode = !frzMode;
 
-    // 2. Sequencer Step Advancing (Internal Tempo vs Analog External CLK)
     bool hasClock = inputs[CLK_INPUT].isConnected();
     bool stepTriggered = false;
 
@@ -37,8 +34,7 @@ void Soundscapes::processSequencer(const ProcessArgs& args) {
             if (hasClock && stepTriggered) {
                 currentSteps[chan] = (currentSteps[chan] + 1) % loopLength[chan];
             } else if (!hasClock) {
-                // Internal Clock
-                float bpm = params[RATE_PARAM].getValue() * 180.0f + 40.0f; // 40 to 220 BPM
+                float bpm = params[RATE_PARAM].getValue() * 180.0f + 40.0f;
                 playheadPhases[chan] += (bpm / 60.0f) * args.sampleTime;
                 if (playheadPhases[chan] >= 1.0f) {
                     playheadPhases[chan] -= 1.0f;
@@ -48,25 +44,21 @@ void Soundscapes::processSequencer(const ProcessArgs& args) {
         }
     }
 
-    // 3. Capture Physical "Fader / Knob Wiggles" (Malekko Live Automation)
     if (playMode) {
         if (focusedChannel == -1) {
-            // Global Fader Routing
             for (int i = 0; i < 8; i++) {
                 float faderVal = params[FADER_1_PARAM + i].getValue();
                 if (focusedFX != -1) {
                     channelFxSends[focusedFX][i] = faderVal;
                 } else if (shftMode) {
-                    // Global Shift: Scale overall channel volumes
                     channelAmplitudes[i] = faderVal;
                 } else {
                     channelAmplitudes[i] = faderVal;
                 }
             }
         } else {
-            // Focused Channel Lockout routing
             for (int i = 0; i < 8; i++) {
-                if (i != focusedChannel) continue; // Lockout inactive sliders
+                if (i != focusedChannel) continue;
                 
                 float faderVal = params[FADER_1_PARAM + i].getValue();
                 int currentActiveStep = currentSteps[focusedChannel];
@@ -74,9 +66,8 @@ void Soundscapes::processSequencer(const ProcessArgs& args) {
                 if (probMode) {
                     stepProbability[focusedChannel][currentActiveStep] = faderVal;
                 } else if (shftMode) {
-                    // Turn Dynamics knob while focused to alter loop length
                     float dynamicsVal = params[DYNAMICS_PARAM].getValue();
-                    loopLength[focusedChannel] = (int)(dynamicsVal * 15.f) + 1; // 1 to 16 steps
+                    loopLength[focusedChannel] = (int)(dynamicsVal * 15.f) + 1;
                 } else {
                     stepVelocity[focusedChannel][currentActiveStep] = faderVal;
                 }
